12e4a6c697dcfe2819b82c6be446adbcfda018cd
mosesprabu <mosesprabu001@gmail.com>
2023-08-16 15:10:15 +0100

    second page added

---
+++ lib/second.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MpGit extends StatelessWidget {
+  const MpGit({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
